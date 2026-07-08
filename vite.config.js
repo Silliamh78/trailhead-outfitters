9bcd4310cf8e9784dad237ab853fa953d6c5dc63
silliamh78 <har20041@byui.edu>
2026-07-08 09:55:03 -0600

    fixed vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // if using React; remove if not needed
+
 export default defineConfig({
-  // Add options here as your needs grow
+  plugins: [react()],
+  // add other config options here
 })
