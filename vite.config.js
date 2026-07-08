8396b7f82bb8de9ec0fdab3c07a92f0e3b9959c0
silliamh78 <har20041@byui.edu>
2026-07-08 10:03:45 -0600

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // if using React; remove if not needed
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  // add other config options here
-})
+  server: {
+    open: true,
+  },
+});
+// // vite.config.js
+// import { defineConfig } from 'vite'
+// export default defineConfig({
+//   // Add options here as your needs grow
+// })
+
+// // vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react' // if using React; remove if not needed
+
+// export default defineConfig({
+//   plugins: [react()],
+//   // add other config options here
+// })
+
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   base: "/",
+// });
